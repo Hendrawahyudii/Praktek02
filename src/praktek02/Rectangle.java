1257df370e3557dfcb8d5c8c1a58d6382fcd3716
Hendrawahyudii <32281589+Hendrawahyudii@users.noreply.github.com>
2018-03-31 14:36:34 +0700

    Membuat attribut dan Behavior

--- src/praktek02/Rectangle.java
+++ src/praktek02/Rectangle.java
@@ -1,5 +1,14 @@
 package praktek02;
 
 public class Rectangle {
+    Integer Harga_bebek;
+    Integer Jumlah_beli;
+    
+    void cetakInfo(){
+        System.out.println("============================");
+        System.out.println("Herga Bebek :"+Harga_bebek);
+        System.out.println("Jumlah Beli :"+Jumlah_beli);
+        System.out.println("============================");
+    }
     
 }
