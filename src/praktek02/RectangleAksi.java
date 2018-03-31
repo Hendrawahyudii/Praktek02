d209ba989f98f5ae97f137933c37aaa85dc15abe
Hendrawahyudii <32281589+Hendrawahyudii@users.noreply.github.com>
2018-03-31 14:44:16 +0700

    Mengisi Nilai Atribut

--- src/praktek02/RectangleAksi.java
+++ src/praktek02/RectangleAksi.java
@@ -3,6 +3,8 @@ package praktek02;
 public class RectangleAksi {
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle();
+        r1.Harga_bebek = 35000;
+        r1.Jumlah_beli = 5;
     }
     
 }
