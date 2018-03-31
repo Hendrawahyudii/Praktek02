21431ed022703543dec9478680671d63054248a9
Hendrawahyudii <32281589+Hendrawahyudii@users.noreply.github.com>
2018-03-31 15:16:02 +0700

    Conctructor - Sama Dengan Nama Class

--- src/praktek02/Rectangle.java
+++ src/praktek02/Rectangle.java
@@ -3,6 +3,11 @@ package praktek02;
 public class Rectangle {
     Integer Harga_bebek;
     Integer Jumlah_beli;
+
+    public Rectangle() {
+        Harga_bebek=40000;
+        Jumlah_beli=2;
+    }
     
     void cetakInfo(){
         System.out.println("============================");
